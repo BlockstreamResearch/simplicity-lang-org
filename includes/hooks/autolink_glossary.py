33f6bcd08587343906bd260cc021ad1ebaeaa3e8
Breno Brito <brenorb@gmail.com>
2026-09-17 11:58:02 -0300

    fix: remove build hook side effect

--- includes/hooks/autolink_glossary.py
+++ includes/hooks/autolink_glossary.py
@@ -12,10 +12,7 @@ def on_page_markdown(markdown, page, config, files):
     # 2. (Any path that ends in glossary.md#some-anchor)
     # 3. Negative lookahead to ensure we don't add { data-preview } if it's already there
 
-    open("/tmp/foobaz", "w").write("hello")
-    
     pattern = r'(\[.*?\]\((?:.*?\/)?glossary\.md#.*?\))(?!\s*\{)'
     
     # Replacement appends the attribute
     return re.sub(pattern, r'\1{ data-preview }', markdown)
-
