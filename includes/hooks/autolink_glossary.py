6ad299fb431d873e47c2fd48f0eab7c59ae9e48e
Seth Schoen <schoen@loyalty.org>
2026-09-18 11:09:38 -0700

    Remove ancient debugging test code

--- includes/hooks/autolink_glossary.py
+++ includes/hooks/autolink_glossary.py
@@ -12,8 +12,6 @@ def on_page_markdown(markdown, page, config, files):
     # 2. (Any path that ends in glossary.md#some-anchor)
     # 3. Negative lookahead to ensure we don't add { data-preview } if it's already there
 
-    open("/tmp/foobaz", "w").write("hello")
-    
     pattern = r'(\[.*?\]\((?:.*?\/)?glossary\.md#.*?\))(?!\s*\{)'
     
     # Replacement appends the attribute
